0b97dd98496884c6b9457d82cce9bacc96be096f
Chris Leonard <chleonar@redhat.com>
2026-09-17 20:38:38 +0000

    migrating DeepGEMM off of pybind and onto TORCH_LIBRARY on vllm's dev branch. Will need to figure out how to handle DeepJIT.
    Signed-off-by: Chris Leonard <chleonar@redhat.com>

--- csrc/python_api.cpp
+++ csrc/python_api.cpp
@@ -1,8 +1,9 @@
-#include <pybind11/pybind11.h>
-#include <torch/python.h>
+#include <torch/all.h>
+#include <torch/library.h>
+#include "torch_library_utils.hpp"
+#include "utils/registration.h"
 
 #include <deep_jit/backend/cuda/backend.hpp>
-#include <deep_jit/python_api.hpp>
 
 #include "apis/config.hpp"
 #include "apis/attention.hpp"
@@ -15,29 +16,4 @@
 #include "apis/mega_mhc.hpp"
 #include "apis/mega_gate.hpp"
 
-#ifndef TORCH_EXTENSION_NAME
-#define TORCH_EXTENSION_NAME _C
-#endif
-
-// ReSharper disable once CppParameterMayBeConstPtrOrRef
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.doc() = "DeepGEMM C++ library";
-
-    // Register JIT objects
-    deep_jit::register_python_api(m, deep_gemm::jit);
-
-    // Register config APIs
-    deep_gemm::config::register_apis(m);
-
-    // Register kernels
-    // TODO: make SM80 incompatible issues raise errors
-    deep_gemm::attention::register_apis(m);
-    deep_gemm::einsum::register_apis(m);
-    deep_gemm::hyperconnection::register_apis(m);
-    deep_gemm::gemm::register_apis(m);
-    deep_gemm::layout::register_apis(m);
-    deep_gemm::mega::register_apis(m);
-    deep_gemm::nvfp4_mega::register_apis(m);
-    deep_gemm::mega_mhc::register_apis(m);
-    deep_gemm::mega_gate::register_apis(m);
-}
+REGISTER_EXTENSION(_C_extension)
